4c81ec13d736adc39f6978b5859474140edf2952
Yanghyeonyong <didgusdyd2@gmail.com>
2026-01-30 17:31:36 +0900

    [Edit] 플레이어 충돌 시 데미지 입는 함정 구현

---
+++ Assets/Scripts/Hyeonyong/TrapObject.cs
@@ -0,0 +1,54 @@
+using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+public class TrapObject : MonoBehaviour
+{
+    //[SerializeField] LayerMask playerLayerMask;
+    //[SerializeField] string playerLayerName = "Player";
+    //int playerLayer = -1;
+    [SerializeField] float damage;
+    [SerializeField] float damageCoolTime = 0f;
+    WaitForSeconds damageCoolTimeWait;
+    bool onDamage=false;
+    Coroutine checkOnDamageCoroutine;
+    private void Awake()
+    {
+        damageCoolTimeWait = new WaitForSeconds(damageCoolTime);
+        //playerLayer =LayerMask.NameToLayer(playerLayerName);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //변신 이전엔 레이어가 달라서 그냥 pc 여부로 판단
+        PlayerController pc =other.GetComponent<PlayerController>();
+        if(pc == null)
+            return;
+        Debug.Log("플레이어 접촉 pc 보유");
+        if (pc.photonView.IsMine)
+        {
+            Debug.Log("플레이어 접촉 pc 보유 내 포톤 뷰");
+            if (!onDamage)
+            {
+                pc.TakeDamage(damage);
+                Debug.Log("플레이어 접촉 pc 보유 내 포톤 뷰 맞았다");
+                checkOnDamageCoroutine =StartCoroutine(CheckOnDamage());
+            }
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        if (checkOnDamageCoroutine != null)
+        {
+            StopCoroutine(checkOnDamageCoroutine);
+        }
+    }
+    IEnumerator CheckOnDamage()
+    {
+        onDamage = true;
+        yield return damageCoolTimeWait;
+        onDamage = false;
+        checkOnDamageCoroutine = null;
+    }
+}
