8c323cb090df2ee2bc09eb3888cdb83467c2d258
Yanghyeonyong <didgusdyd2@gmail.com>
2026-01-31 17:47:45 +0900

    [Edit] TrapObject NPC도 데미지 입도록 수정

--- Assets/Scripts/Hyeonyong/TrapObject.cs
+++ Assets/Scripts/Hyeonyong/TrapObject.cs
@@ -8,47 +8,55 @@ public class TrapObject : MonoBehaviour
     //[SerializeField] string playerLayerName = "Player";
     //int playerLayer = -1;
     [SerializeField] float damage;
-    [SerializeField] float damageCoolTime = 0f;
-    WaitForSeconds damageCoolTimeWait;
-    bool onDamage=false;
-    Coroutine checkOnDamageCoroutine;
-    private void Awake()
-    {
-        damageCoolTimeWait = new WaitForSeconds(damageCoolTime);
-        //playerLayer =LayerMask.NameToLayer(playerLayerName);
-    }
+    //[SerializeField] float damageCoolTime = 0f;
+    //WaitForSeconds damageCoolTimeWait;
+    //bool onDamage=false;
+    //Coroutine checkOnDamageCoroutine;
+    //private void Awake()
+    //{
+    //    damageCoolTimeWait = new WaitForSeconds(damageCoolTime);
+    //    //playerLayer = LayerMask.NameToLayer(playerLayerName);
+    //}
     private void OnTriggerEnter(Collider other)
     {
-        //변신 이전엔 레이어가 달라서 그냥 pc 여부로 판단
-        PlayerController pc =other.GetComponent<PlayerController>();
-        if(pc == null)
+        IDamageable damageable = other.GetComponent<IDamageable>();
+        if (damageable == null)
             return;
-        Debug.Log("플레이어 접촉 pc 보유");
-        if (pc.photonView.IsMine)
-        {
-            Debug.Log("플레이어 접촉 pc 보유 내 포톤 뷰");
-            if (!onDamage)
-            {
-                pc.TakeDamage(damage);
-                Debug.Log("플레이어 접촉 pc 보유 내 포톤 뷰 맞았다");
-                checkOnDamageCoroutine =StartCoroutine(CheckOnDamage());
-            }
-        }
-
+        damageable.TakeDamage(damage);
+        //변신 이전엔 레이어가 달라서 그냥 pc 여부로 판단
+        //PlayerController pc =other.GetComponent<PlayerController>();
+        //if (pc != null)
+        //{
+        //    Debug.Log("플레이어 접촉 pc 보유");
+        //    if (pc.photonView.IsMine)
+        //    {
+        //        Debug.Log("플레이어 접촉 pc 보유 내 포톤 뷰");
+        //        if (!onDamage)
+        //        {
+        //            pc.TakeDamage(damage);
+        //            Debug.Log("플레이어 접촉 pc 보유 내 포톤 뷰 맞았다");
+        //            checkOnDamageCoroutine = StartCoroutine(CheckOnDamage());
+        //        }
+        //    }
+        //}
+        //else
+        //{
+            
+        //}
     }
 
-    private void OnDisable()
-    {
-        if (checkOnDamageCoroutine != null)
-        {
-            StopCoroutine(checkOnDamageCoroutine);
-        }
-    }
-    IEnumerator CheckOnDamage()
-    {
-        onDamage = true;
-        yield return damageCoolTimeWait;
-        onDamage = false;
-        checkOnDamageCoroutine = null;
-    }
+    //private void OnDisable()
+    //{
+    //    if (checkOnDamageCoroutine != null)
+    //    {
+    //        StopCoroutine(checkOnDamageCoroutine);
+    //    }
+    //}
+    //IEnumerator CheckOnDamage()
+    //{
+    //    onDamage = true;
+    //    yield return damageCoolTimeWait;
+    //    onDamage = false;
+    //    checkOnDamageCoroutine = null;
+    //}
 }
